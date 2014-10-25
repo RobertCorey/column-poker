8d232afbccb3a0d4aa92291f3e78c513aff1b4f8
Robert Corey <rcorey@ziji-media.com>
2014-10-24 20:39:30 -0400

    Change to deck to simple object instead of array

--- src/deck.js
+++ src/deck.js
@@ -1,12 +1,18 @@
-// Build pristine deck
+/* Exports an unshuffled 52 card deck of cards in the form
+* card: {rank: Q, suit: s}
+*/
 var _ = require('lodash');
 var RANKS = [2, 3, 4, 5, 6, 7, 8, 9, 'T', 'J', 'Q', 'K', 'A'],
 SUITS = ['s', 'c', 'd', 'h'],
 deck = [];
 
+
 _.forEach(RANKS, function (rank) {
   _.forEach(SUITS, function (suit) {
-    deck.push(rank + suit);
+    var card = {};
+    card.rank = rank;
+    card.suit = suit;
+    deck.push(card);
   });
 });
 
